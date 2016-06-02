505e152120fac2586bdf6129c2af9ee81eca99c9
Man Hoang Xuan <man.hoangxuan@harveynash.vn>
2016-06-02 14:15:39 +0700

    Update data for category table

--- dien_tu_ung_dung.sql
+++ dien_tu_ung_dung.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2016 at 02:16 AM
+-- Generation Time: Jun 02, 2016 at 09:13 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -33,7 +33,18 @@ CREATE TABLE IF NOT EXISTS `danh_muc` (
   `mo_ta` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `so_linh_kien` int(11) NOT NULL,
   PRIMARY KEY (`ma_danh_muc`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `danh_muc`
+--
+
+INSERT INTO `danh_muc` (`ma_danh_muc`, `ten_danh_muc`, `chuc_nang`, `mo_ta`, `so_linh_kien`) VALUES
+(1, 'Điện trở', 'Cản trở dòng điện', 'Điện trở là một đại lượng vật lí biểu thị đặc tính cản trở dòng điện của một vật có khả năng cho dòng điện chạy qua. Vật nào dẫn điện càng tốt thì điện trở của nó càng nhỏ và ngược lại.', 20),
+(3, 'Tụ điện', 'Lọc nhiễu', 'Tụ điện là một hệ hai vật dẫn đặt gần nhau và ngăn cách nhau bằng một lớp cách điện. Tụ điện dùng để chứa điện tích. Hình ảnh một số loại tụ sử dụng trong mạch điện xoay chiều. Tụ điện phẳng là dụng cụ được dùng phổ biến trong mạch điện xoay chiều và các mạch vô tuyến điện', 15),
+(4, 'Transistor', 'Công tắc điện tử', 'Transistor hay tranzito là một loại linh kiện bán dẫn chủ động, thường được sử dụng như một phần tử khuếch đại hoặc một khóa điện tử.', 6),
+(6, 'Điốt', 'Dẫn 1 chiều', 'Điốt là linh kiện bán dẫn đầu tiên. Khả năng chỉnh lưu của tinh thể được nhà vật lý người Đức Ferdinand Braun phát hiện năm 1874. Điốt bán dẫn đầu tiên được phát triển vào khoảng năm 1906 được làm từ các tinh thể khoáng vật như galena.', 3),
+(7, 'Led', 'Phát sáng', 'LED (viết tắt của Light Emitting Diode, có nghĩa là diode phát quang), là các diod có khả năng phát ra ánh sáng hay tia hồng ngoại, tử ngoại. Công nghệ LED là công nghệ chiếu sáng bằng 2 điện cực với sự hỗ trợ của các loại vật liệu bán dẫn và công nghệ nano.', 4);
 
 -- --------------------------------------------------------
 
